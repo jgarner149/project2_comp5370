c706a7b3cacb022ada42a9dae9f794018e871a83
Justin Garner <54516067+jgarner149@users.noreply.github.com>
2020-10-05 11:46:55 -0500

    Add files via upload

---
+++ alert.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<script>
+  alert("I highly recommend you find another bank because your bank’s website is insecure.");
+</script>
+
+</body>
+</html>
+<!-- http://127.0.0.1/alert.php -->
---
+++ vulnerable.php
@@ -0,0 +1,61 @@
+<?php
+    $name = $_REQUEST ['customer'];
+?>
+<html>
+<head>
+<style>
+
+th, td {
+  padding: 5px;
+}
+th {
+  text-align: left;
+}
+</style>
+</head>
+<body>
+
+<h2>Customer: <?php echo $customer; ?></h2>
+<p>Account: Your Account</p>
+
+<table style="width:100%">
+  <tr>
+    <th>Date</th>
+    <th>Vendor</th> 
+    <th>Amount</th>
+  </tr>
+  <tr>
+    <td>14Sept2020</td>
+    <td>Starbucks</td>
+    <td>($5.74)</td>
+  </tr>
+  <tr>
+    <td>14Sept2020</td>
+    <td>Pizza Hut</td>
+    <td>($17.93)</td>
+  </tr>
+  <tr>
+    <td>15Sept2020</td>
+    <td>Hardee`s</td>
+    <td>($8.37)</td>
+  </tr>
+  <tr>
+    <td>15Sept2020</td>
+    <td>Taco Bell</td>
+    <td>($6.18)</td>
+  </tr>
+  <tr>
+    <td>16Sept2020</td>
+    <td>Arby`s</td>
+    <td>($9.53)</td>
+  </tr>
+  <tr>
+    <td>16Sept2020</td>
+    <td>ABC Store</td>
+    <td>($32.94)</td>
+  </tr>
+</table>
+
+</body>
+</html>
+<!-- http://127.0.0.1/vulnerable.php?customer=Bob -->
